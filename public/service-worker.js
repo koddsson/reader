48f5f0dc3575d0deecee81044c8441cfb48c1684
Kristján Oddsson <koddsson@gmail.com>
2018-09-20 18:46:11 +0100

    open page on notification click

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,3 +4,21 @@ self.addEventListener('push', function(event) {
   }
   event.waitUntil(self.registration.showNotification('Reader', options))
 })
+
+self.onnotificationclick = function(event) {
+  event.notification.close()
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(
+    clients.matchAll({type: 'window'}).then(function(clientList) {
+      for (const i = 0; i < clientList.length; i++) {
+        const client = clientList[i]
+        if (client.url == '/' && 'focus' in client) {
+          return client.focus()
+        }
+      }
+      if (clients.openWindow) return clients.openWindow('/')
+    })
+  )
+}
